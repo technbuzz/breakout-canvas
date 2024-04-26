8a9c08abc0ae78cb57ea91efbace79287280763e
technbuzz <samiullah.khan.1989@gmail.com>
2024-04-26 15:05:12 +0400

    bounce of walls

--- scripts/Ball.js
+++ scripts/Ball.js
@@ -1,20 +1,34 @@
 import { Point } from "./utils.js";
 export class Ball extends Point {
-  constructor(x, y ) {
+  constructor(x, y, radius=5) {
     super(x, y);
+    this.radius = radius;
     this.velocity = new Point(1, -1)
   }
 
   drawBall() {
     globalThis.game.c.beginPath();
-    globalThis.game.c.arc(this.x, this.y, 5, 0, Math.PI * 2);
+    globalThis.game.c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     globalThis.game.c.fillStyle = "red";
     globalThis.game.c.fill();
     globalThis.game.c.closePath();
   }
 
+  bounceOffWalls() {
+    if(this.x > globalThis.game.width - this.radius || this.x < this.radius) {
+      this.velocity.x = -this.velocity.x;
+    }
+
+    if(this.y < this.radius || this.y > globalThis.game.height - this.radius) {
+      this.velocity.y = -this.velocity.y;
+    }
+  }
+
   draw() {
     this.drawBall()
+
+    this.bounceOffWalls()
+
     this.x += this.velocity.x;
     this.y += this.velocity.y;
   }
